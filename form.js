af5d633c48231054ec795739b1bedd6cbc185ff7
Glo <glo.movil@gmail.com>
2023-08-25 00:33:38 +0200

    table created dynamically with inputs storing values in localstore

--- form.js
+++ form.js
@@ -1,36 +1,102 @@
-const checkbox = document.getElementById('checa');
-const company = document.getElementById('company');
-const mail = document.getElementById('mail');
-const contactName = document.getElementById('contactName');
+const numberRows = 4;
 
+const newInput = (nameId, placeholderId, container, type) => {
+    const inputItem = document.createElement("input");
+    inputItem.type = type;
+    inputItem.id = nameId;
+    inputItem.placeholder = placeholderId;
+    container.appendChild(inputItem);
+}
+
+//create table
+function createTableInputs(numRows) {
+    const table = document.createElement("table");
+
+    const headerRow = table.insertRow();
+
+    titleColN = document.createElement("th");
+    titleColN.textContent = "Company name";
+    headerRow.appendChild(titleColN);
+
+    titleColE = document.createElement("th");
+    titleColE.textContent = "Email";
+    headerRow.appendChild(titleColE);
+
+    titleColT = document.createElement("th");
+    titleColT.textContent = "Telephone";
+    headerRow.appendChild(titleColT);
+
+
+    for (let i = 1; i <= numRows; i++) {
+        const row = table.insertRow();
+
+        const cellN = row.insertCell();
+        newInput("nombreCoIp" + i, "... nombre", cellN, "text");
+
+        const cellE = row.insertCell();
+        //cellE.setAttribute("id", `e${i}`);
+        newInput("mailIp" + i, "... email", cellE, "email");
+
+        const cellT = row.insertCell();
+        // cellT.setAttribute("id", `t${i}`);
+        newInput("phoneIp" + i, "... phone", cellT, "text");
+    }
+    document.body.appendChild(table);
+
+}
+
+createTableInputs(numberRows);
+
+
+
+/* 
+//create inputs
+newInput("nombreCo", "... nombre", "#nombreTd", "text");
+newInput("mail", "... e-mail", "#mailTd", "email");
+newInput("phone", "... phone", "#telephoneTd", "text");
+ */
+
+//retrieve texts data from local storage
+function getResultLS(nameElementId) {
+    const resultStored = localStorage.getItem(nameElementId);
+    document.getElementById(nameElementId).value = resultStored || "";
+}
+
+for (let i = 1; i <= numberRows; i++) {
+    getResultLS("nombreCoIp" + i);
+    getResultLS("mailIp" + i);
+    getResultLS("phoneIp" + i);
+
+}
+
+
+//localstorage to save textual inputs
+function saveResultLS(e, nameId) {
+    const subject = e.target.value;
+    const name = e.target.id;
+    //console.log({ subject });
+    localStorage.setItem(name, subject);
+}
+
+for (let i = 1; i <= numberRows; i++) {
+    document.getElementById('nombreCoIp' + i).addEventListener('change', (e) => saveResultLS(e, this.id));
+    document.getElementById('mailIp' + i).addEventListener('change', (e) => saveResultLS(e, this.id));
+    document.getElementById('phoneIp' + i).addEventListener('change', (e) => saveResultLS(e, this.id));
+}
+
+
+/* 
+//CHECKBOX
+const checkbox = document.getElementById('checa');
 // Load the checkbox state from local storage on page load
 const savedValue = localStorage.getItem('checaChecked');
 if (savedValue === 'true') {
     checkbox.checked = true;
 }
-
 // Add an event listener to the checkbox to save its state in local storage
 checkbox.addEventListener('change', function () {
     localStorage.setItem('checaChecked', this.checked);
 });
+ */
 
-//retrieve texts data from local storage
-function getResult(nameElementId) {
-    const resultStored = localStorage.getItem(nameElementId.id);
-    nameElementId.value = resultStored;
-}
-getResult(company);
-getResult(mail);
-getResult(contactName);
 
-//localstorage to save textual inputs
-function saveResult(e, nameId) {
-    const subject = e.target.value;
-    const name = e.target.id;
-    console.log({ subject });
-    localStorage.setItem(name, subject);
-}
-
-company.addEventListener('change', (e) => saveResult(e, this.id));
-mail.addEventListener('change', (e) => saveResult(e, this.id));
-contactName.addEventListener('change', (e) => saveResult(e, this.id));
